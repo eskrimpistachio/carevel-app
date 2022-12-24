0e38dc711941ebb1838e8affa3bf1fab0ba66dbb
fatih <alfatihrizqialfian@gmail.com>
2022-12-24 21:55:04 +0700

    all commit

---
+++ app/Http/Controllers/CreateController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\create;
+use App\Http\Requests\StorecreateRequest;
+use App\Http\Requests\UpdatecreateRequest;
+
+class CreateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorecreateRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorecreateRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\create  $create
+     * @return \Illuminate\Http\Response
+     */
+    public function show(create $create)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\create  $create
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(create $create)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatecreateRequest  $request
+     * @param  \App\Models\create  $create
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatecreateRequest $request, create $create)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\create  $create
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(create $create)
+    {
+        //
+    }
+}
